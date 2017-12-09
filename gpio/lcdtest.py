aff2ceaf322cab3e77e92ccea04748e0d542eebf
jonoreilly <jonoreilly007@gmail.com>
2017-12-09 16:59:00 +0100

    aaa

--- gpio/lcdtest.py
+++ gpio/lcdtest.py
@@ -2,5 +2,8 @@ import lcdlib
 
 print ("all set")
 while True:
-    try:
-        exec(input("\nclearscreen() or writestring(thestring):\n>"))
+	try:
+		exec(input("\nclearscreen() or writestring(thestring):\n>"))
+	except ValueError:
+		print ("fail")
+
